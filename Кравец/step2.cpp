bde15f0e8782ef006924ca9dd5878417933a8815
KravezDV <Kravez-2003@mail.ru>
2020-04-13 11:28:12 +0300

    Create step2.cpp

---
+++ Кравец/step2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int step(int a, int b) {
+	int f;
+	f = a;
+	for (int i = 2; i <= b; i++) {
+		f = f * a;
+	}
+	return f;
+}
+
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	
+	cout << step(a,b);
+}
+
