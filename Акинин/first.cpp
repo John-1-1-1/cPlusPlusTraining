78d4f53473c86eaa2ff1b0cc66ceae7e34bf1831
JATOn37 <alexakin2002@gmail.com>
2020-04-13 15:27:15 +0700

    Create first.cpp

---
+++ Акинин/first.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int paw(int f, int a) {
+
+	int f1 = 1;
+
+	for (int i = 0; i < a; i++) {
+		f1 = f * f1;
+	}
+	
+	return f1;
+}
+
+int main()
+{
+	int m, n;
+	cin >> m >> n;
+	cout << paw(m, n);
+}
