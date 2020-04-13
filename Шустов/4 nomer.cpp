923bb23729780251a99fbd9767ba4a3819b8a5b4
Ioshito <ioshito@inbox.ru>
2020-04-13 16:22:44 +0700

    4 nomer

---
+++ Шустов/4 nomer.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+void banan(int* a, int* b) {
+    int temp;
+    temp = *a; // int* a 
+    *a = *b;
+    *b = temp;
+}
+
+int main()
+{
+    int a, b;
+    cout << "Vvedite dva chisla: ";
+    cin >> a >> b;
+    banan(&a, &b);
+    cout << "Peremennaja a: " << a << ", peremennaja b: " << b;
+}
