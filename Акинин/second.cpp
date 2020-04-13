19fa79e01f0131e699a2e77a0c8372a4a7481423
JATOn37 <alexakin2002@gmail.com>
2020-04-13 15:55:25 +0700

    Create second.cpp

---
+++ Акинин/second.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+char trans(char b[], int f) {
+	char temp;
+	for (int i = 0; i < f/2; i++) {
+		temp = b[i];
+		b[i] = b[f-1 - i];
+		b[f-1 - i] = temp;
+	}
+
+	for (int i = 0; i < f; i++) {
+		cout << b[i];
+	}
+
+	return b[f];
+}
+
+int main() {
+	const int N = 7;
+	char a[N] = "abcdef";
+	trans(a, N);
+}
