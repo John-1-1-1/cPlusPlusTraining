c66814b1b4711c3dc9d23b9f6ed4e3e9b22f1ccf
JATOn37 <alexakin2002@gmail.com>
2020-04-13 16:18:00 +0700

    Create third.cpp

---
+++ Акинин/third.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int fun(char str1[]) {
+	int count = 0;
+	for (int i = 0; str1[i] != '\0'; i++) {
+		count++;
+	}
+
+	return count;
+}
+
+int main() {
+	char str[] = "jatonchik";
+	cout << fun(str);
+}
