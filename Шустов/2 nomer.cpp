d1291beb687ea3a3d38336f077596129e3fe0f67
Ioshito <ioshito@inbox.ru>
2020-04-13 15:27:51 +0700

    2 nomer

---
+++ Шустов/2 nomer.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+string stroka(string a) {
+    string b;
+    for (int i = a.length(); i >= 0; i--) {
+        b = b + a[i];
+    }
+    return b;
+}
+
+int main()
+{
+    string a;
+    cout << "Vvedite stroku dla perevorota: ";
+    cin >> a;
+    cout << "Stroka:" << stroka(a);
+}
