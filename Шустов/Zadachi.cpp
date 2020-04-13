fae5efe911c5e81159cd5907faf263c713d73867
Ioshito <ioshito@inbox.ru>
2020-04-13 14:30:33 +0700

    1 nomer

---
+++ Шустов/Zadachi.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int step(int a, int b) {
+    return pow(a, b);
+}
+
+int main()
+{
+    int a, b;
+    cout << "Введите число, которое хотите возвести в степень: ";
+    cin >> a;
+    cout << "Введите степень: ";
+    cin >> b;
+    cout << "Возведение: " << step(a, b);
+}
